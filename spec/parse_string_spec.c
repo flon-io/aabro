2217efb0eef43fce8f3290100502ed3c7b8608c6
John Mettraux <jmettraux@gmail.com>
2013-12-17 23:09:50 +0900

    scaffold .h and .c

---
+++ spec/parse_string_spec.c
@@ -0,0 +1,18 @@
+
+//
+// specifying aabro
+//
+// Tue Dec 17 22:56:18 JST 2013
+//
+
+#include "aabro.h"
+
+
+describe "abr_parse_string()"
+{
+  it "flips burgers"
+  {
+    ensure(1 == 2);
+  }
+}
+
