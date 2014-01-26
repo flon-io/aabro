be044b2f2e1b4c10d7cca8f4ba35e3c02adcf025
John Mettraux <jmettraux@gmail.com>
2014-01-26 16:11:54 +0900

    simplify specs thanks to rodzo's before/after/===f

--- spec/node_spec.c
+++ spec/node_spec.c
@@ -10,28 +10,31 @@
 
 describe "abr_node"
 {
+  before each
+  {
+    abr_node *n = NULL;
+  }
+  after each
+  {
+    if (n != NULL) abr_free_node(n);
+  }
+
   describe "abr_malloc_node()"
   {
     it "creates a node"
     {
-      abr_node *n = abr_malloc_node("nada", 0);
+      n = abr_malloc_node("nada", 0);
 
       ensure(n != NULL);
-
-      abr_free_node(n);
     }
   }
   describe "abr_node_to_string(n)"
   {
     it "returns a string representation of the node"
     {
-      abr_node *n = abr_malloc_node("nada", 0);
-      char *s = abr_node_to_string(n);
+      n = abr_malloc_node("nada", 0);
 
-      ensure(strcmp("[ nada, 0 ]", s) == 0);
-
-      free(s);
-      abr_free_node(n);
+      ensure(abr_node_to_string(n) ===f "[ nada, 0 ]");
     }
   }
 }
