835bd47fa26a5a6b6367cab66c5b3b5e1ae05623
John Mettraux <jmettraux@gmail.com>
2014-02-10 06:38:18 +0900

    explore variadic functions for parsers

--- scratch/pad.c
+++ scratch/pad.c
@@ -29,3 +29,69 @@ abr_parse_node {
 
 abr_node *abr_parse(abr_parse_node *root, char *input); // signature
 
+// ***
+
+// __func__ (c99)
+// printf("func is >%s<\n", __func__);
+
+abr_node *abr_parse_string(abr_input *i, char *name, ...);
+void abr_parse_string(abr_context *c, char *name, ...);
+void abr_parse_string(abr_context *c, ...);
+void abr_name(abr_context *c, ...);
+
+typedef void abr_parser(abr_context *c, ...);
+
+// but
+abr_parse_or(c, my_parse_x, my_parse_y, NULL);
+abr_parse_or(c, my_parse_x, my_parse_y, abr_parse_over);
+// "moindre mal" ?
+
+
+//    parser do
+//
+//      value ==
+//        spaces? +
+//        (object | array | string | number | btrue | bfalse | null) +
+//        spaces?
+//
+//      spaces? == _("\s\n\r") * 0
+//
+//      object == `{` + (entry + (`,` + entry) * 0) * 0 + `}`
+//      entry == spaces? + string + spaces? + `:` + value
+//
+//      array == `[` + (value + (`,` + value) * 0) * 0 + `]`
+//
+//      string == `"` + ((`\\` + _) | _('^"')) * 0 + `"`
+//
+//      _digit == _("0-9")
+//
+//      number ==
+//        `-` * -1 +
+//        (`0` | (_("1-9") + _digit * 0)) +
+//        (`.` + _digit * 1) * -1 +
+//        (_("eE") + _("+-") * -1 + _digit * 1) * -1
+//
+//      btrue == `true`
+//      bfalse == `false`
+//      null == `null`
+//    end
+void my_parse_val(abr_context *c, ...)
+{
+  abr_parse_or(
+    c,
+    my_parse_object,
+    my_parse_array,
+    my_parse_string,
+    // ...
+    NULL);
+}
+void my_parse_value(abr_context *c, ...)
+{
+  abr_parse_sequence(
+    c,
+    my_parse_optional_spaces,
+    my_parse_val,
+    my_parse_optional_spaces,
+    NULL);
+}
+
