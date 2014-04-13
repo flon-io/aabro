33903a821e48c794cb93e995245b0a78e7cd6e5d
John Mettraux <jmettraux@gmail.com>
2014-04-14 06:35:32 +0900

    introduce spec about the parser composite

---
+++ spec/parser_spec.c
@@ -0,0 +1,51 @@
+
+//
+// specifying aabro
+//
+// Mon Apr 14 06:28:07 JST 2014
+//
+
+#include "aabro.h"
+
+
+context "parser"
+{
+  before each
+  {
+    abr_tree *t = NULL;
+    abr_parser *p = NULL;
+  }
+  after each
+  {
+    if (t != NULL) abr_tree_free(t);
+    if (p != NULL) abr_parser_free(p);
+  }
+
+  it "parses everything"
+  {
+    p = abr_rep(abr_string("x"), 1, 4);
+    t = abr_parse("x", 0, p);
+    char *s = abr_tree_to_string(t);
+
+    ensure(s ===f ""
+      "[ null, 1, 0, 1, [\n"
+      "  [ null, 1, 0, 1, [] ],\n"
+      "  [ null, 0, 1, -1, [] ]\n"
+      "] ]");
+  }
+
+  it "fails when there is too much to parse"
+  {
+    p = abr_rep(abr_string("x"), 1, 2);
+    t = abr_parse("xxy", 0, p);
+    char *s = abr_tree_to_string(t);
+
+    ensure(s ===f ""
+      //"[ null, 1, 0, 2, [\n"
+      "[ null, 0, 0, 2, [\n"
+      "  [ null, 1, 0, 1, [] ],\n"
+      "  [ null, 1, 1, 1, [] ]\n"
+      "] ]");
+  }
+}
+
