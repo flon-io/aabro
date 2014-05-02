7653ab35314f04773940e4c29f44ea6437802345
John Mettraux <jmettraux@gmail.com>
2014-05-03 06:34:34 +0900

    comment out the sample parser for now
    needs vspec zeroed

--- spec/sample_arith_spec.c
+++ spec/sample_arith_spec.c
@@ -46,21 +46,21 @@ context "sample, arith"
     //abr_parser *expression =
     //  abr_name("expression", operation);
 
-    abr_parser *val =
-      abr_n_alt(
-        "val",
-        abr_regex("^-?[0-9]+"),
-        abr_seq(abr_string("("), abr_n("exp"), abr_string(")"), NULL),
-        NULL);
-    abr_parser *op =
-      abr_n_seq(
-        "op",
-        abr_n("exp"),
-        abr_regex("^[\+\-\*\/]"),
-        abr_n("exp"),
-        NULL);
-    abr_parser *exp =
-      abr_n_alt("exp", op, val, NULL);
+    //abr_parser *val =
+    //  abr_n_alt(
+    //    "val",
+    //    abr_regex("^-?[0-9]+"),
+    //    abr_seq(abr_string("("), abr_n("exp"), abr_string(")"), NULL),
+    //    NULL);
+    //abr_parser *op =
+    //  abr_n_seq(
+    //    "op",
+    //    abr_n("exp"),
+    //    abr_regex("^[\+\-\*\/]"),
+    //    abr_n("exp"),
+    //    NULL);
+    //abr_parser *exp =
+    //  abr_n_alt("exp", op, val, NULL);
   }
 
   it "parses numbers"
