ac0430b27c9e35fc7ae1d4a48c0140cdb5d1da6c
John Mettraux <jmettraux@gmail.com>
2015-06-07 14:27:43 +0900

    bring back commented out EOS line

--- src/aabro.c
+++ src/aabro.c
@@ -850,6 +850,7 @@ static fabr_tree *rex_seq(fabr_input *i, char *rx, size_t rxn)
 
   while (1)
   {
+    //if (*(i->string + i->offset) == '\0') break; // EOS
     if (rx_at(crx, crxn, 0) == '\0') break;
 
     *next = rex_rep(i, crx, crxn);
