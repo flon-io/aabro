e6df1dbe61493510ae939848275b4a6453c9a62e
John Mettraux <jmettraux@gmail.com>
2015-06-13 21:20:50 +0900

    explore first combo spec

--- spec/combinations_spec.c
+++ spec/combinations_spec.c
@@ -21,18 +21,25 @@ context "combinations"
     fabr_tree_free(t);
   }
 
-//  static fabr_tree *_cbo_empty(fabr_input *i)
-//  {
-//    return fabr_rex("e", i, "[ \t]*");
-//  }
-//  static fabr_tree *_cbo_empties(fabr_input *i)
-//  {
-//    return fabr_rep("es", i, _cbo_empty, 1, 0);
-//  }
+  static fabr_tree *_cbo_text_line(fabr_input *i)
+  {
+    return fabr_rex("txl", i, "[ ]*[a-z]+");
+  }
+  static fabr_tree *_cbo_blank_line(fabr_input *i)
+  {
+    return fabr_rex("bll", i, "[ \t]*");
+  }
 
   describe "empty string vs 0 or more"
   {
     it "suceeds"
+    {
+      fabr_input i = { "abc", 0, 0 };
+
+      t = fabr_alt("l", &i, _text_line, _blank_line, NULL);
+
+      fabr_puts_tree(t, s, 1);
+    }
 //    {
 //      char *s = "";
 //
