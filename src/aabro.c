ea2d2a7f611c5c2a99eab50a2565d1429b550d00
John Mettraux <jmettraux@gmail.com>
2015-07-03 06:02:23 +0900

    refine the new fabr_eseq()

--- src/aabro.c
+++ src/aabro.c
@@ -1101,8 +1101,8 @@ fabr_tree *fabr_eseq(
     {
       fabr_tree_free(eltt);
     }
-    if (rr == -1) { r->result == -1; break; }
-    if (rr == 0) { break; }
+    if (rr == 0 && (jseq == 0 && j == 0)) { break; }
+    if (rr != 1) { r->result = rr; break; }
 
     // separator
 
@@ -1119,44 +1119,9 @@ fabr_tree *fabr_eseq(
     {
       fabr_tree_free(sept);
     }
-    if (rr == -1) { r->result == -1; break; }
+// TODO injext ll-based logic
+    if (rr == -1) { r->result = -1; break; }
     if (rr == 0) { break; }
-
-//    if ((jseq || j == 0) && eltt->result == 0) { break; }
-//    //
-//    if (eltt->result != 1) { r->result = eltt->result; break; }
-//    if (sept->result == -1) { r->result = -1; break; }
-//    if (sept->result == 0) { break; }
-
-//    short jj = j % 2;
-//
-//    fabr_tree *t = ps[jj](i);
-//    fabr_tree **n = next;
-//    *next = t;
-//    next = &t->sibling;
-//
-//    if (t->result == -1) { r->result = -1; break; }
-//
-//    if (jj == 1) empty_sep = (t->result == 1 && t->length == 0);
-//
-//    if (t->result == 0)
-//    {
-//      if (jj == 0 && (jseq || j > 0) && empty_sep == 0)
-//        // elt (not first elt) missing
-//      {
-//        r->result = 0;
-//      }
-//      else if (i->flags & FABR_F_PRUNE)
-//        // no separator but prune
-//      {
-//        *n = NULL;
-//        next = n;
-//        fabr_tree_free(t);
-//      }
-//      break;
-//    }
-//
-//    r->length += t->length;
   }
 
   if (r->result == 1 && endp)
