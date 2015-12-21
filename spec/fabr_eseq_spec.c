6ec72296751ee4bc0f30f73dbf8e7335fd592536
John Mettraux <jmettraux@gmail.com>
2015-12-22 06:13:32 +0900

    fill "parses <>" spec

--- spec/fabr_eseq_spec.c
+++ spec/fabr_eseq_spec.c
@@ -352,6 +352,19 @@ describe "fabr_eseq()"
   context "with zero-length elements and separators"
   {
     it "parses <>"
+    {
+      i.string = "<>";
+      i.flags = FABR_F_PRUNE;
+
+      t = fabr_eseq("B", &i, _es_sta, _es_el, _es_se, _es_end);
+
+      ensure(fabr_tree_to_string(t, i.string, 0) ===f ""
+        "[ \"B\", 1, 0, 2, null, \"eseq\", 0, [\n"
+        "  [ null, 1, 0, 1, null, \"str\", 1, \"<\" ],\n"
+        "  [ \"e\", 1, 1, 0, null, \"rex\", 2, \"\" ],\n"
+        "  [ null, 1, 1, 1, null, \"str\", 1, \">\" ]\n"
+        "] ]");
+    }
 
     it "parses <a,,a>"
     {
